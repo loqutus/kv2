0ea6133cb11a26be7f0366cd0cb55f6a0d5b97ee
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2022-07-08 00:36:42 +0200

    commit

--- pkg/server/server/info.go
+++ pkg/server/server/info.go
@@ -2,12 +2,17 @@ package server
 
 import (
 	"fmt"
+	"os"
 )
 
 // Info is a function that retunrs server info.
 func (s *Server) Info() (string, error) {
 	var info string
+	hostname, err := os.Hostname()
 	info = "id: " + s.id
+	if err == nil {
+		info += "\nhostname: " + hostname
+	}
 	info += "\nkeys: " + fmt.Sprint(len(s.kv))
 	info += "\nmemory: " + fmt.Sprint(bToMb(s.memUsage)) + " MB"
 	info += "\nmemory limit: " + fmt.Sprint(bToMb(s.memLimit)) + " MB"
