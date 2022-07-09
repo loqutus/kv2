29a40989ec81a0c86b05e0bb449d1e403b3e0214
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2022-07-09 15:21:31 +0200

    commit

--- pkg/server/server/client_handler.go
+++ pkg/server/server/client_handler.go
@@ -10,6 +10,7 @@ import (
 // ServerHandler is the main server handler.
 func (s *Server) ClientHandler(conn net.Conn) {
 	logrus.Println("client handler")
+	defer conn.Close()
 	for {
 		c, err := s.GetCmd(conn)
 		if err == io.EOF {
