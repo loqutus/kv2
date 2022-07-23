bf8feebd231407d51fb9dfa9f8c8d084ff344666
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2022-07-24 00:53:48 +0200

    commit

--- pkg/server/server/pyroscope.go
+++ pkg/server/server/pyroscope.go
@@ -4,9 +4,11 @@ import (
 	"os"
 
 	pyroscope "github.com/pyroscope-io/pyroscope/pkg/agent/profiler"
+	"github.com/sirupsen/logrus"
 )
 
 func startPyroscope() {
+	logrus.Println("Starting Pyroscope")
 	pyroscope.Start(pyroscope.Config{
 		ApplicationName: os.Getenv("kv2"),
 		ServerAddress:   os.Getenv("KV2_PYROSCOPE_SERVER"),
